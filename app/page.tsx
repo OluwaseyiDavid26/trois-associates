58d45e3d1e29db246ebcdb8a48fd7cd999f9ca52
Oluwaseyi David <oluwaseyidavid26@gmail.com>
2026-07-21 12:20:07 -0700

    dealer

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Hero from "@/components/Home/Hero";
 import TrustStrip from "@/components/Home/Truststrip";
 import Services from "@/components/Home/Services";
 import Truststrip from "@/components/Home/Truststrip";
+import WhoWeAre from "@/components/Home/WhoWeAre";
+import WhyChooseUs from "@/components/Home/Whychooseus";
 
 export default function HomePage() {
   return (
@@ -9,6 +11,8 @@ export default function HomePage() {
       <Hero />
       <Truststrip />
       <Services />
+      <WhyChooseUs />
+      <WhoWeAre />
     </>
   );
 }
